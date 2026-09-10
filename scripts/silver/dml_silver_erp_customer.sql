8892bd05e88582ec9a705d40a5bbfb052742a509
Harshith495 <87695636+Harshith495@users.noreply.github.com>
2026-09-10 18:19:08 +1000

    Inserting the data from bronze layer erp_customer to silver layer erp_customer

---
+++ scripts/silver/dml_silver_erp_customer.sql
@@ -0,0 +1,24 @@
+-- Inserting the data from bronze layer erp_customer to silver layer erp_customer
+
+INSERT into silver.erp_cust_az12 (
+    cid,
+    bdate,
+    gen
+)
+
+select 
+case when cid like 'NAS%' then substring(cid, 4, len(cid))
+    else cid 
+end as cid,
+case when bdate >getdate() then null 
+else bdate 
+end as bdate,
+  -- following the proper case for every row
+case when upper(trim(gen)) in ('F', 'FEMALE') THEN 'Female'
+    when upper(trim(gen)) in ('M', 'MALE') THEN 'Male'
+    else 'n/a'
+end as gen
+from bronze.erp_cust_az12;
+
+
+select * from silver.erp_cust_az12;
