713ba5dd71a134abe3dc7548afbc914bb940b015
Harshith495 <87695636+Harshith495@users.noreply.github.com>
2026-09-11 18:11:54 +1000

    Inserting the category data from the bronze layer to the silver layer.

---
+++ scripts/silver/dml_silver_erp_px_cat_g1v2.sql
@@ -0,0 +1,15 @@
+-- Inserting the category data from the bronze layer to the silver layer.
+TRUNCATE TABLE silver.erp_px_cat_g1v2;
+insert into silver.erp_px_cat_g1v2 (
+    id,
+    cat,
+    subcat,
+    maintenance
+)
+select 
+id,
+cat,
+subcat,
+maintenance
+from bronze.erp_px_cat_g1v2;
+
